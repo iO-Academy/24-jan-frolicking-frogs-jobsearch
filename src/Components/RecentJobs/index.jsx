629413f97215d9356c3eed8167de013de4c5ace9
Connor Yeeles <connoryeeles@outlook.com>
2024-02-13 10:54:01 +0000

    Added styling for recent jobs component

--- src/Components/RecentJobs/index.jsx
+++ src/Components/RecentJobs/index.jsx
@@ -3,9 +3,11 @@ import RecentBox from "./RecentBox"
 function RecentJobs() {
 
     return (
-        <div class='bg-white'>
-            <h3>Most Recent jobs</h3>
-            <a href="#">View all jobs</a>
+        <div class='bg-white flex flex-col px-20'>
+            <div class='flex flex-row justify-around'>
+                <h3 class='text-4xl'>Most recent jobs</h3>
+                <a href="#" class='text-sky-600'>View all jobs →</a> 
+            </div>
             <RecentBox />
 
             
