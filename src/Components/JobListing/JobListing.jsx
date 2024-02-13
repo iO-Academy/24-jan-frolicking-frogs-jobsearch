068a61e08a39a9b48f13b3c4608633054a9fa484
Alex Livingston <aslivingston@hotmail.co.uk>
2024-02-13 18:09:38 +0000

    added job listing per id data using params

--- src/Components/JobListing/JobListing.jsx
+++ src/Components/JobListing/JobListing.jsx
@@ -5,31 +5,63 @@ function JobListing() {
 
     const {id} = useParams()
 
-
-    const [jobInfo, setJobInfo] = useState([])
+    const [jobCompany, setJobCompany] = useState('')
+    const [jobTitle, setJobTitle] = useState('')
+    const [jobType, setJobType] = useState('')
+    const [jobLogo, setJobLogo] = useState('')
+    const [jobDesc, setJobDesc] = useState('')
+    const [jobSalary, setJobSalary] = useState('')
+    const [jobDatePosted, setDatePosted] = useState('')
+    
 
     useEffect(getJobInfo, [])
 
     function getJobInfo() {
-        fetch ('https://job-search-api.dev.io-academy.uk/jobs')
+        fetch (`https://job-search-api.dev.io-academy.uk/jobs/${id}`)
         .then(function (response) {
             return response.json() 
         }).then(function (data) {
             console.log(data)
-            setJobInfo(data)
+            setJobCompany(data.company)
+            setJobTitle(data.job_title)
+            setJobType(data.type)
+            setJobLogo(data.logo)
+            setJobDesc(data.job_description)
+            setJobSalary(data.salary)
+            setDatePosted(data.posted)
         })
     }
 
     return (
         <>
-            {/* {jobInfo.map(function(job) {
-                return (
-
-                    
-                )
-            })} */}
-            {id}
-            
+            <div>
+                {jobTitle}
+                {jobType}
+            </div>
+            <div>
+                {id}
+                <img src={jobLogo} alt="" />
+                {jobCompany}
+            </div>
+            <div>
+                <div>
+                    £{jobSalary}
+                </div>
+                <div>
+                    {jobType}
+                </div>
+                <div>
+                    {jobDatePosted}
+                </div>
+            </div>
+            <div>
+                <h3>Job Description:</h3>
+                {jobDesc}
+            </div>
+            <div>
+                <button>Apply Now</button>
+                <button>Save for later</button>
+            </div>
         </>
     )
 }
