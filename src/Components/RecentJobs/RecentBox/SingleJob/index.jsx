26077a96dc4c2f7629bd1a8cec17fc848308baf3
Connor Yeeles <connoryeeles@outlook.com>
2024-02-13 12:09:40 +0000

    Improved table layout

--- src/Components/RecentJobs/RecentBox/SingleJob/index.jsx
+++ src/Components/RecentJobs/RecentBox/SingleJob/index.jsx
@@ -3,15 +3,17 @@ function SingleJob({logo, JobTitle, company, type, salary}) {
     return (
 
         <tr>
-                <td>
-                    <div class='flex'>
-                        <img src={logo} alt="logo of company"/>
-                        {JobTitle}                    
+            <td>
+                <div class='flex'>
+                    <img src={logo} alt="logo of company" class='h-10 w-10'/>
+                    <div class='flex flex-col'>
+                        {JobTitle}
+                        {company}                         
                     </div>
-                </td>
-                <td>{company}</td>
-                <td>{type}</td>
-                <td>{salary}</td>
+                </div>
+            </td>
+            <td>{type}</td>
+            <td>{salary}</td>
         </tr>
 
     )
