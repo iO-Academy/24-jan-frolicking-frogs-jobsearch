1a4b5cd7e12c943d3ff382055258320fd863732d
Victoria McCusker <vrmm@hotmail.co.uk>
2024-02-13 11:04:23 +0000

    adding skills fetch

--- src/Components/RecentJobs/RecentBox/SingleJob/index.jsx
+++ src/Components/RecentJobs/RecentBox/SingleJob/index.jsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react"
+
 function SingleJob({logo, JobTitle, company, type, salary, skills}) {
+
+    const [skillsButton, setSkillsButton] = useState([])
+    useEffect(getSkillsButton, [])
+
+    function getSkillsButton() {
+        fetch (skills)
+        .then(function (response) {
+            return response.json() 
+        }).then(function (data) {
+            console.log(data.skills)
+            setSkillsButton(data.skills)
+        })
+    }
 
     return (
 
@@ -9,10 +24,13 @@ function SingleJob({logo, JobTitle, company, type, salary, skills}) {
                 <li>{company}</li>
                 <button className="bg-green-200">{type}</button>
                 <li>{salary}</li>
-                <li>{skills}</li>
-
                 </div>
         </div>
+
+
+
+
+
 
     )
 }
