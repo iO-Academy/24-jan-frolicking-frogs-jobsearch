3b328d6dd1442e3db4db5f8849977409c402f3f4
Connor Yeeles <connoryeeles@outlook.com>
2024-02-13 11:05:30 +0000

    Improved spacing on recent jobs layout

--- src/Components/RecentJobs/index.jsx
+++ src/Components/RecentJobs/index.jsx
@@ -4,7 +4,7 @@ function RecentJobs() {
 
     return (
         <div class='bg-white flex flex-col px-20'>
-            <div class='flex flex-row justify-around'>
+            <div class='flex flex-row justify-between py-10'>
                 <h3 class='text-4xl'>Most recent jobs</h3>
                 <a href="#" class='text-sky-600'>View all jobs →</a> 
             </div>
