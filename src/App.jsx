bcb9a0e62ce868498ee90d330c88358f71f0d2e3
Victoria McCusker <vrmm@hotmail.co.uk>
2024-02-14 16:59:37 +0000

    adding navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Link, NavLink, Route, Routes } from 'react-router-dom'
 import './App.css'
 import RecentJobs from './Components/RecentJobs'
 import JobListing from './Components/JobListing/JobListing'
@@ -11,7 +11,12 @@ function App() {
     
       <BrowserRouter>
 
-      
+        <nav className="flex flex-row bg-slate-500 justify-center gap-5 text-white p-2">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/">Full Time</NavLink>
+          <NavLink to="/">Part Time</NavLink>
+          <NavLink to="/">Contract</NavLink>
+        </nav>
         {/* <Link to='/joblisting'>Jobs</Link> */}
 
         <Routes>
