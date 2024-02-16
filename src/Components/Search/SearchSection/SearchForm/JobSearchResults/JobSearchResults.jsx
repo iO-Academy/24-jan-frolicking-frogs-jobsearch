0cc497848d222a99412b3b3b32b38e3d257385ba
rosina <rosemat563@gmail.com>
2024-02-16 12:56:16 +0000

    adding no results found when no results from search

--- src/Components/Search/SearchSection/SearchForm/JobSearchResults/JobSearchResults.jsx
+++ src/Components/Search/SearchSection/SearchForm/JobSearchResults/JobSearchResults.jsx
@@ -14,6 +14,8 @@ function JobSearchResults() {
             <div className='flex flex-col justify-between py-10'>
                 <div className='flex flex-col py-5 justify-between items-baseline'>
                     
+                {searchInfo.length!=0 ?
+                <>
                     <div className="flex items-baseline gap-80">
                         <h3 className='text-4xl font-semibold pb-5'>Search Results</h3>
                         <a href="/" className='text-sky-600'>View Most Recent jobs →</a>
@@ -30,10 +32,12 @@ function JobSearchResults() {
                     </thead>
                     <tbody>
                         {searchInfo.map(function(job) {
-                            return <SingleJobSearch key={job.id} jobTitle={job.job_title} company={job.company} salary={job.salary} type={job.type} logo={job.logo} id={job.id} skills={job.skills}/>
+                            return <SingleJobSearch key={job.id} jobTitle={job.job_title} company={job.company} salary={job.salary} type={job.type} logo={job.logo} id={job.id} skills={job.skills}/> 
                         })}
                     </tbody>
                     </table>
+                    </>
+                    : <div className="text-2xl">No Results Found</div>}
                 </div>
             </div>
         </div>
