cccdcf927efd2f042cbc23bac2b028084c4279ef
rosina <rosemat563@gmail.com>
2024-02-16 10:06:28 +0000

    styling the nav bar functionalities

--- src/Components/Search/Contract/Contract.jsx
+++ src/Components/Search/Contract/Contract.jsx
@@ -17,12 +17,34 @@ function Contract() {
     }
 
     return (
-
-        <div>
-            {contract.map(function(job) {
-                return <FullTimeSingle key={job.id} jobTitle={job.job_title} company={job.company} salary={job.salary} type={job.type} logo={job.logo} />
-            })}
+        <div className='bg-white flex justify-center mx-10 lg:m-0'>
+        <div className='flex flex-col justify-between py-10'>
+            <div className='flex flex-col py-5 justify-between items-baseline'>
+               
+                <div className="flex items-baseline gap-80">
+                    <h3 className='text-4xl font-semibold pb-5'>Contract Jobs</h3>
+                    <a href="/" className='text-sky-600'>View Most Recent jobs →</a>
+                </div>
+                
+                <table className='border-separate-collapse table-auto w-full text-white'>
+                    <thead>
+                        <tr className='text-left bg-gray-700'>
+                            <th className='pl-5'>Job title / Company</th>
+                            <th className='px-5'>Type</th>
+                            <th className='px-5'>Salary</th>
+                            <th className='px-5'>Skills</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {contract.map(function(job) {
+                    return <FullTimeSingle key={job.id} jobTitle={job.job_title} company={job.company} salary={job.salary} type={job.type} logo={job.logo} />
+                })}
+                    </tbody>
+                </table>
+                </div>
+            </div>
         </div>
+        
 
     )
 }
